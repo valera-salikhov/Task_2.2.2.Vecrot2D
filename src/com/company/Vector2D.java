27c73cdb41c987c295b7f7d7302546a2e5bcd0d3
valera-salikhov <valera-salikhov@mail.ru>
2020-12-21 17:03:21 +0300

    first task

---
+++ src/com/company/Vector2D.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Locale;
+
+public class Vector2D {
+    public double vX, vY;
+
+    public Vector2D() {
+        vX = 1;
+        vY = 1;
+    }
+
+    public Vector2D(double X, double Y) {
+        vX = X;
+        vY = Y;
+    }
+
+    public Vector2D(Vector2D v) {
+        this.vX = v.vX;
+        this.vY = v.vY;
+    }
+
+    public void print() {
+        System.out.println("(" + String.format(Locale.US, "%.2f, %.2f", this.vX, this.vY) + ")");
+    }
+
+}
